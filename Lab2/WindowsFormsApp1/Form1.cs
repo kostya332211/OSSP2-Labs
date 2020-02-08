a5acfe7c56e5d424b8f9b1f167f02770a73f5cf1
Develer12 <cenia-v@mail.ru>
2020-02-08 17:01:18 +0300

    Completed

---
+++ Lab2/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using ActiveXCPP2Lib;
+using AxActiveXCPP2Lib;
+
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if(axActiveXCPP21.Size.Width == axActiveXCPP21.Size.Height)
+                MessageBox.Show("Circle radius:" + axActiveXCPP21.TestMyMethod(axActiveXCPP21.Size.Width));
+            else
+                MessageBox.Show("It's not a Circle. Radius:" + axActiveXCPP21.Size);
+        }
+
+    }
+}
